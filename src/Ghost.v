1e0bebbb46a2395ebf46489cdffda97344e242d2
Yuanlin Lin <im.yuanlinlin@gmail.com>
2021-01-06 20:42:16 +0800

    不随机了啦 直接开数组

--- src/Ghost.v
+++ src/Ghost.v
@@ -3,21 +3,44 @@ module Ghost_M(
     input wire rst,               // Reset
     output reg [9:0] x,           // X value of Ghost's position 
     output reg [8:0] y,           // Y value of Ghost's position
-	 output result,
-    output reg [1:0] direction
+	output result,
+    output reg [1:0] direction,
+	output wire [1:0] nextDir;
 );
 
 	wire [31:0] clkdiv;
 	clkdiv c0(.clk(clk), .rst(rst), .clkdiv(clkdiv));
 	CheckCollision cc_turn(.clk(clk), .rst(rst), .PacX(x), .PacY(y), .state(direction), .result(result));
 
+	reg [1:0] randomDir [15:0];
+	reg [3:0] randomCnt;
+	
+	assign nextDir = randomDir[randomCnt];
+	
 	initial begin
- 		direction <= 2'b00;
+		randomCnt <= 2'd0;
+		randomDir[0] <= 2'd0;
+		randomDir[1] <= 2'd2;
+		randomDir[2] <= 2'd1;
+		randomDir[3] <= 2'd3;
+		randomDir[4] <= 2'd2;
+		randomDir[5] <= 2'd3;
+		randomDir[6] <= 2'd1;
+		randomDir[7] <= 2'd0;
+		randomDir[8] <= 2'd1;
+		randomDir[9] <= 2'd0;
+		randomDir[10] <= 2'd3;
+		randomDir[11] <= 2'd2;
+		randomDir[12] <= 2'd3;
+		randomDir[13] <= 2'd1;
+		randomDir[14] <= 2'd2;
+		randomDir[15] <= 2'd0;
+	end
+	
+	initial begin
     	x <= 10'd200;
     	y <= 9'd146;
-		
-		randomCnt1 <= 2'b01;
-		randomCnt2 <= 3'b010;
+ 		direction <= 2'b00;
   	end
 
 	always@(posedge clkdiv[17])begin
@@ -34,20 +57,12 @@ module Ghost_M(
 			endcase
 		end
 	end
-
-	reg [1:0] randomCnt1;	
-	always @ (posedge clkdiv[7]) begin
-		randomCnt1 <= randomCnt1 + 1;
-	end
-
-	reg [2:0] randomCnt2;	
-	always @ (posedge clkdiv[8]) begin
-		randomCnt2 <= randomCnt2 + 1;
-	end
   
 	always @ (posedge clkdiv[9]) begin
-	    if(result == 0)
-			direction <= randomCnt1 + randomCnt2;
+	    if(result == 0) begin
+			direction <= nextDir;
+			randomCnt <= randomCnt + 1;
+		end
  	end
 
 endmodule
