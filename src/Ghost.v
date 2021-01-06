060d68abb48999dd419b040b20b3671e0630d88a
Yuanlin Lin <im.yuanlinlin@gmail.com>
2021-01-06 19:34:14 +0800

    🧹 整理代码

--- src/Ghost.v
+++ src/Ghost.v
@@ -10,36 +10,34 @@ module Ghost_M(
 
 	wire [31:0] clkdiv;
 	clkdiv c0(.clk(clk), .rst(rst), .clkdiv(clkdiv));
-   CheckCollision cc_turn(.clk(clk), .rst(rst), .PacX(x), .PacY(y), .state(direction), .result(result));
+	CheckCollision cc_turn(.clk(clk), .rst(rst), .PacX(x), .PacY(y), .state(direction), .result(result));
 
-  // Initialize Position
-  initial begin
-    direction <= 2'b00;
-	 nextDir <= 2'b01;
-    x <= 10'd200;
-    y <= 9'd146;
-  end
+	initial begin
+ 		direction <= 2'b00;
+		nextDir <= 2'b01;
+    	x <= 10'd200;
+    	y <= 9'd146;
+  	end
 
 	always@(posedge clkdiv[17])begin
 		if(result != 0)begin
 			case(direction)
 				2'b10:
-						x <= x - 10'd1;
+					x <= x - 10'd1;
 				2'b11:
-						x <= x + 10'd1;
+					x <= x + 10'd1;
 				2'b00:
-						y <= y - 9'd1;
+					y <= y - 9'd1;
 				2'b01:
-						y <= y + 9'd1;
+					y <= y + 9'd1;
 			endcase
 		end
 	end
   
-  always @ (posedge clkdiv[9]) begin
-	 nextDir <= nextDir + 1;
-    if(result == 0) begin
-		direction <= nextDir;
-    end 
-  end
+	always @ (posedge clkdiv[9]) begin
+		nextDir <= nextDir + 1;
+	    if(result == 0)
+			direction <= nextDir;
+ 	end
 
 endmodule
